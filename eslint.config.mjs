a9134d4195440c08a0d96333863f13f9a91c806d
Dobbymin <gmin990624@gmail.com>
2025-09-11 17:39:41 +0900

    chore: eslint 규칙 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import jsxA11y from "eslint-plugin-jsx-a11y";
+import prettier from "eslint-plugin-prettier";
 import unusedImports from "eslint-plugin-unused-imports";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -12,14 +13,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:prettier/recommended"),
   {
     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
     plugins: {
       "unused-imports": unusedImports,
       "jsx-a11y": jsxA11y,
+      prettier,
     },
     rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
       "react/jsx-sort-props": [
         "warn",
         {
@@ -32,6 +37,7 @@ const eslintConfig = [
       "jsx-a11y/alt-text": "warn",
       "jsx-a11y/click-events-have-key-events": "warn",
       "jsx-a11y/no-static-element-interactions": "warn",
+      "unused-imports/no-unused-imports": "warn",
       "prettier/prettier": ["warn", { usePrettierrc: false }],
     },
     settings: {
