7a7becdb1a2b7e8cfa09da057c6940989035cead
Dobbymin <gmin990624@gmail.com>
2025-09-13 03:03:44 +0900

    feat: 모든 클립 불러오기 api 코드 구현

--- src/entities/clip/apis/all-clips.api.ts
+++ src/entities/clip/apis/all-clips.api.ts
@@ -1,3 +1,5 @@
+import { BASE_URL } from "@/shared";
+
 import { ClipContents, Pageable, Sort } from "../types";
 
 export interface AllClipsResponse {
@@ -17,4 +19,24 @@ export interface GetAllClipsParameter {
   size: number;
 }
 
-export const ALL_CLIPS_PATH = "/api/clips";
+export const ALL_CLIPS_PATH = `${BASE_URL}/api/clips`;
+
+export const allClipsAPI = async ({ lastCreatedAt, size }: GetAllClipsParameter): Promise<AllClipsResponse | null> => {
+  try {
+    const params = new URLSearchParams({
+      lastCreatedAt,
+      size: String(size),
+    });
+
+    const url = `${ALL_CLIPS_PATH}?${params.toString()}`;
+    const response = await fetch(url);
+
+    if (!response.ok) throw new Error(response.statusText);
+
+    return await response.json();
+  } catch (error) {
+    console.error("allClipAPI error", error);
+
+    return null;
+  }
+};
