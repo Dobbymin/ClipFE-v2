8e90aec1d49f53bcd18e58ebf46711a42893e0cd
Dobbymin <gmin990624@gmail.com>
2025-09-13 10:17:37 +0900

    fix(root): 웹 접근성 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='ko'>
       <body className={`${pretendard.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}>
         <QueryClientProvider>
           <MSWComponent />
