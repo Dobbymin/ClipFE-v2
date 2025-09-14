b8656ca1663f6aacdf546b57c0ea4539f2cfca92
Dobbymin <gmin990624@gmail.com>
2025-09-14 21:25:02 +0900

    feat: 메인페이지 헤더 추가

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,14 @@
+import { Header } from "@/widgets";
+
 type Props = {
   children: React.ReactNode;
 };
 
 export default function layout({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 }
