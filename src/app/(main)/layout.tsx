9ef2edff097405d6383ff10796ca70abe570f113
Dobbymin <gmin990624@gmail.com>
2025-09-15 01:37:09 +0900

    fix: main layout 수정

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,15 @@
-import { Header } from "@/widgets";
+import { Header, NavigateBar } from "@/widgets";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function layout({ children }: Props) {
+export default function MainLayout({ children }: Props) {
   return (
-    <div>
+    <div className='relative flex min-h-screen w-full flex-col'>
       <Header />
-      {children}
+      <main className='flex w-full grow flex-col pt-2 pb-24'>{children}</main>
+      <NavigateBar />
     </div>
   );
 }
