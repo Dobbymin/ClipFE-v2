17d4501d1ed036885d60870b0b17f9caf788c0f0
Dobbymin <gmin990624@gmail.com>
2025-09-14 14:24:28 +0900

    fix: clip logo 이미지 파일 경로 변경

--- src/features/signup/ui/section/SignupTitleSection.tsx
+++ src/features/signup/ui/section/SignupTitleSection.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 
-import ClipLogo from "@/shared/_assets/logo/clip_3d.webp";
-
 export const SignupTitleSection = () => {
   return (
     <div className='flex w-full flex-col items-center justify-center'>
-      <Image alt='clip-logo' className='h-auto py-8' src={ClipLogo} width={100} />
+      <Image alt='clip-logo' className='py-8' height={100} src='/logo/clip_3d.webp' width={100} />
       <div className='mb-4 text-lg font-bold text-neutral-700 dark:text-neutral-300'>
         지금 가입하고 첫 링크를 <span className='font-benviitLasik text-clip-primary'>Clip</span> 해보세요!
       </div>
