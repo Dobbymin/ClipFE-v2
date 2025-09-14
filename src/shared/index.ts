d90ff76b09e67945e4e9cd4b72c2a9ae5b913185
Dobbymin <gmin990624@gmail.com>
2025-09-14 14:18:58 +0900

    feat(login): 소셜 로그인 주소 추가

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,3 +3,5 @@ export * from "./constants";
 export * from "./libs";
 export * from "./mocks";
 export * from "./providers";
+export * from "./utils";
+export * from "./store";
---
+++ src/shared/libs/social-login-path.ts
@@ -0,0 +1,5 @@
+import { BASE_URL } from "../libs";
+
+export type Platform = "kakao" | "naver" | "google";
+
+export const SOCIAL_LOGIN_PATH = (platform: Platform) => `${BASE_URL}/oauth2/authorization/${platform}`;
