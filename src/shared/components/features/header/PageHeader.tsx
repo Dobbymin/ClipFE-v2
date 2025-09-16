68c71f2e86b4f6277e4cfb7a415d1da42ac59145
Dobbymin <gmin990624@gmail.com>
2025-09-16 23:29:59 +0900

    Revert "refactor: 버튼을 link로 변경"
    This reverts commit 56893072896520789ed4da932696036d65d497f6.

--- src/shared/components/features/header/PageHeader.tsx
+++ src/shared/components/features/header/PageHeader.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 import { ChevronLeft } from "lucide-react";
 
-import { ROUTER_PATH } from "../../../constants";
+import { Button } from "../../ui";
 
 type Props = {
   title?: string;
 };
 
 export const PageHeader = ({ title }: Props) => {
+  const router = useRouter();
+
   return (
     <header className='flex h-16 w-full items-center justify-start gap-2 p-4'>
-      <Link className='flex cursor-pointer items-center justify-center' href={ROUTER_PATH.MAIN}>
+      <Button
+        aria-label='뒤로 가기'
+        className='flex cursor-pointer items-center justify-center'
+        variant='ghost'
+        onClick={() => router.back()}
+      >
         <ChevronLeft className='size-6' />
-      </Link>
+      </Button>
       <div className='flex items-center'>
         <h1 className='text-xl font-semibold'>{title}</h1>
       </div>
