ab03942deaedb3bc7e8314d3611d7a18db9d3c5c
Dobbymin <gmin990624@gmail.com>
2025-09-16 22:24:16 +0900

    feat: 이미지 오류 임시 해결

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: false,
+	/* TODO: 추후 이미지 proxy 기술로 변경 */
+	images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'velog.velcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.wishket.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
