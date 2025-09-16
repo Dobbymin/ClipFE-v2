ba6301339a4c444fac9338de427fde72871a97d9
Dobbymin <gmin990624@gmail.com>
2025-09-16 22:43:07 +0900

    fix: 최근 클립 카드 스켈레톤 코드 적용

--- src/features/main/ui/RecentClipSection.tsx
+++ src/features/main/ui/RecentClipSection.tsx
@@ -1,35 +1,35 @@
 "use client";
 
-import { useGetAllClips } from "@/entities";
-import { ClipCard } from "@/shared";
-import { ChevronRight } from "lucide-react";
+import { useEffect, useState } from "react";
 
-import { ClipCardSkeleton } from "../components";
+import { ClipCard, RECENT_CLIP_MOCK } from "@/shared";
+import { ChevronRight } from "lucide-react";
 
 type Props = {
   nickname: string;
 };
 
 export const RecentClipSection = ({ nickname }: Props) => {
-  const { data: recentClipsData, isLoading } = useGetAllClips({
-    lastCreatedAt: "",
-    size: 10,
-  });
+  // const { data: recentClipsData, isLoading } = useGetAllClips({
+  //   lastCreatedAt: "",
+  //   size: 10,
+  // });
+  // TODO: msw 코드 수정 후 다시 query hook 으로 수정
+  const [isLoading, setIsLoading] = useState(true);
+  const recentClipsData = RECENT_CLIP_MOCK;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   if (!recentClipsData) {
     return (
       <div className='flex min-h-[150px] w-full items-center justify-center text-primary'>
         최근 저장한 클립이 없어요. 🧐
-      </div>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <div className='grid w-full auto-cols-[174px] grid-flow-col items-start justify-start gap-4 overflow-x-auto px-2 py-4'>
-        {Array.from({ length: 3 }).map((_, index) => (
-          <ClipCardSkeleton key={index} />
-        ))}
       </div>
     );
   }
@@ -42,19 +42,19 @@ export const RecentClipSection = ({ nickname }: Props) => {
         </div>
         <ChevronRight />
       </div>
-      <div className='scrollbar-hide grid w-full auto-cols-[174px] grid-flow-col items-start justify-start gap-4 overflow-x-auto px-2 py-4'>
-        {recentClipsData.content.map((data) => (
-          <ClipCard
-            key={data.tagId}
-            memo={data.memo}
-            tagName={data.tagName}
-            /* TODO: api 수정 이후 thumbnail 변경 */
-            thumbnail={data.url}
-            title={data.title}
-            url={data.url}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <div className='grid w-full auto-cols-[174px] grid-flow-col items-start justify-start gap-4 overflow-x-auto px-2 py-4'>
+          {Array.from({ length: 3 }).map((_, index) => (
+            <ClipCard.Skeleton key={index} variant='recent' />
+          ))}
+        </div>
+      ) : (
+        <div className='scrollbar-hide grid w-full auto-cols-[174px] grid-flow-col items-start justify-start gap-4 overflow-x-auto px-2 py-4'>
+          {recentClipsData.content.map((data) => (
+            <ClipCard key={data.tagId} variant='recent' {...data} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
