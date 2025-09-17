056a20c6d12380ff945574b338225db31b840420
Dobbymin <gmin990624@gmail.com>
2025-09-17 21:07:57 +0900

    fix: search 페이지를 클립 검색 페이지로 변경

--- src/widgets/layout/navigatebar/NavigateBar.tsx
+++ src/widgets/layout/navigatebar/NavigateBar.tsx
@@ -15,7 +15,7 @@ type ButtonItem = {
 const BUTTON_LIST: ButtonItem[] = [
   { tab: "HOME", icon: Home, text: "HOME", path: ROUTER_PATH.ROOT },
   { tab: "CLIP", icon: Paperclip, text: "CLIP", path: ROUTER_PATH.CLIP },
-  { tab: "SEARCH", icon: FolderSearch, text: "SEARCH", path: ROUTER_PATH.SEARCH },
+  { tab: "SEARCH", icon: FolderSearch, text: "SEARCH", path: ROUTER_PATH.CLIP_SEARCH },
   { tab: "FRIEND_SEARCH", icon: UserSearch, text: "FRIEND", path: ROUTER_PATH.FRIEND_SEARCH },
 ];
 
